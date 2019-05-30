f91feb6047f0a1c61eb4966724ebd08953bb976e
lin <zl1836@nyu.edu>
2019-05-29 22:04:50 -0400

    Icon pack, 网上下了一堆图标，可以随便用 item system：可以简单的创造近的item，item parent class

---
+++ New Unity Project/Assets/Scripts/ItemClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemClass : MonoBehaviour
+{
+    [SerializeField]
+    string names;
+    [SerializeField]
+    Sprite icon;
+
+    public void SetIcon()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = icon;
+    }
+    public void Collect()
+    {
+        this.gameObject.AddComponent<BoxCollider>();
+        this.gameObject.GetComponent<BoxCollider>().size= new Vector3(1, 1, 1);
+        this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<ItemMananger>().recieveItem(this);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/ItemMananger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMananger : MonoBehaviour
+{
+    List<ItemClass> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<ItemClass> items = new List<ItemClass> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecieveItem(ItemClass item) 
+    {
+        items.Add(item);
+    }
+}
