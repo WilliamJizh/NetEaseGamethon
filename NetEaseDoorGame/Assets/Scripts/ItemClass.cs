8c82af6759015f6cb8ab4c23b2af548d0fc464d1
lin <zl1836@nyu.edu>
2019-06-01 22:30:39 -0400

    Add a get player function in Item class, you can now get the player object using this function

--- NetEaseDoorGame/Assets/Scripts/ItemClass.cs
+++ NetEaseDoorGame/Assets/Scripts/ItemClass.cs
@@ -7,28 +7,43 @@ public abstract class ItemClass : MonoBehaviour
     [SerializeField]
     public string names;
     [SerializeField]
+    //2D sprite for icon
     public  Sprite icon;
+    // bool for collected or not
     public bool collected = false;
-    
+    //player gameobject
+    private GameObject player;
+
+    //set the icon for item
     public void SetIcon()
     {
         this.GetComponent<SpriteRenderer>().sprite = icon;
     }
+
+    //get the sprite of the icon
     public Sprite GetIcon()
     {
         return icon;
     }
+
+    //give the item a box collider 
     public void Collect()
     {
         this.gameObject.AddComponent<BoxCollider>();
         this.gameObject.GetComponent<BoxCollider>().size= new Vector3(1, 1, 1);
         this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
     }
+    //return the player object
+    public GameObject getPlayer()
+    {
+        return player;
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            player = other.gameObject;
             collected = true;
             other.GetComponent<ItemMananger>().ReceiveItem(this);
 
