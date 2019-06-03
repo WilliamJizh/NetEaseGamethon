ea1832957ab2fab1f2621749871419e6b8d55c4e
lin <zl1836@nyu.edu>
2019-06-03 17:06:16 -0400

    no message

---
+++ NetEaseDoorGame/Assets/Scripts/CameraPosFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosFollow : MonoBehaviour
+{
+
+    GameObject player;
+    [SerializeField]
+    float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null) {
+
+            Vector3 camerapos = transform.position;
+            camerapos.x = player.transform.position.x;
+            camerapos.z = player.transform.position.z;
+            camerapos.y = height;
+            transform.position = camerapos;
+        }
+    }
+
+    public void GetPlayer(GameObject player) {
+
+        this.player = player;
+       
+    }
+}
