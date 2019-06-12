f4e0045e1546d07adc30cbef2526257ef6f94376
MagicShow1999 <40450775+MagicShow1999@users.noreply.github.com>
2019-06-12 09:58:00 +0800

    roomtype 1

---
+++ NetEaseDoorGame/Assets/Scripts/TreasureBox.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureBox : MonoBehaviour
+{
+
+
+    public GameObject VampireBat, Easter, Exhilarant, Eye, HealthPlus, MilkNBread, Mushroom, SpeedBoost;
+    //temp position used to make it appear in front of the shop object
+    Vector3 temPos;
+
+    //spawn prefabs once per 2 seconds
+    public float spawnRate = 2f;
+
+    //variable to set next spawn time
+    float nextSpawn = 0f;
+
+    //variable to contain random value
+    int whatToSpawn;
+
+    //other variable that is needed
+    GameObject player;
+    PlayerStats playerStats;
+    public bool arrived = false;
+    bool wantbuy = false;
+    void Start()
+    {
+
+
+        //adjust spawn position
+        temPos = transform.position;
+        temPos.z -= 2f;
+        temPos.x -= 2f;
+    }
+
+    void Update()
+    {
+
+        if (arrived)
+        {
+
+            whatToSpawn = Random.Range(1, 8);
+
+
+            SetCollider();
+            arrived = false;
+
+            //NEED TO REVISE PRICE FOR EACH ITEM 
+
+
+            switch (whatToSpawn)
+            {
+                case 1:
+                    Instantiate(VampireBat, temPos, Quaternion.identity);
+                    return;
+                case 2:
+                    Instantiate(Easter, temPos, Quaternion.identity);
+
+                    return;
+                case 3:
+                    Instantiate(Exhilarant, temPos, Quaternion.identity);
+  
+                    return;
+                case 4:
+                    Instantiate(Eye, temPos, Quaternion.identity);
+
+                    return;
+                case 5:
+                    Instantiate(HealthPlus, temPos, Quaternion.identity);
+
+                    return;
+                case 6:
+                    Instantiate(MilkNBread, temPos, Quaternion.identity);
+     
+                    return;
+                case 7:
+                    Instantiate(Mushroom, temPos, Quaternion.identity);
+  
+                    return;
+                case 8:
+                    Instantiate(SpeedBoost, temPos, Quaternion.identity);
+                    return;
+
+                    //If there are more items, add them below and change the random number range.
+            }
+        }
+    }
+
+    //Interact with player if player arrives the shop
+
+    void SetCollider()
+    {
+
+        this.gameObject.AddComponent<BoxCollider>();
+        this.gameObject.GetComponent<BoxCollider>().size = new Vector3(1, 1, 1);
+        this.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+
+            arrived = true;
+            player = other.gameObject;
+            Debug.Log("You opened the treasure box!!");
+            playerStats = player.GetComponent<PlayerStats>();
+
+
+        }
+
+
+    }
+}
